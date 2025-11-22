eebcbbad721e21465dd6d51d76e99737144fbb21
Raine 'Gravecat' Simmons <gc@gravecat.com>
2025-11-22 04:12:07 +0000

    Added a #hash command to generate word hashes.

---
+++ src/parser/cheats.cpp
@@ -0,0 +1,25 @@
+// parser/cheats.cpp -- Cheating/development/testing commands.
+
+// SPDX-FileType: SOURCE
+// SPDX-FileCopyrightText: Copyright 2025 Raine Simmons <gc@gravecat.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include "core/terminal.hpp"
+#include "parser/cheats.hpp"
+
+namespace westgate {
+namespace parser {
+namespace cheats {
+
+// Hashes words into integers.
+void hash(PARSER_FUNCTION)
+{
+    if (words.size() < 2)
+    {
+        terminal::print("{Y}Please specify a word to be hashed.");
+        return;
+    }
+    terminal::print("The hashed version of {C}" + words.at(1) + " {w}is {C}" + std::to_string(words_hashed.at(1)));
+}
+
+} } }   // cheats, parser, westgate namespaces
